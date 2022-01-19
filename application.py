00016189087c2997c5da9458e737a8c52fcd7285
Kiet Diep <kiet.diep@email.ucr.edu>
2022-01-18 23:48:38 -0800

    finished with security

--- application.py
+++ application.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request
-
+import smtplib
+import os
 
 application = Flask(__name__)
+
+gmail_user = os.environ.get('EMAIL_USER')
+gmail_pass = os.environ.get('mongo_pass')
 
 
 @application.route('/')
@@ -30,9 +34,16 @@ def form():
     name = request.form.get("name")
     email = request.form.get("email")
     msg = request.form.get("message")
-    # store into mongoDB
+    email_msg = f"Subject: Connection\n\n{name} @ {email} has sent a message: {msg}"
+    print(email_msg)
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(gmail_user, gmail_pass)
+    server.sendmail(gmail_user, gmail_user, email_msg)
+
     return render_template('form.html', name=name)
 
 
 if __name__ == "__main__":
+
     application.run(debug=True)
